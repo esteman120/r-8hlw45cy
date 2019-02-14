f1a72cd0dd0e70ab00eb7bd7c90d89b7827c19c4
unknown <estebanbotero1036@gmail.com>
2019-02-14 17:03:48 -0500

    Primer montaje

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+      super();
+
+      this.state = {
+        valor:false
+      }
+  }
+
+
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" /> Mostrar información importante</label>
+        <label><input type="checkbox" onClick={this.ocultar.bind(this)}/> Mostrar información importante</label>
+      {
+        this.state.valor ?
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        :
+        null
+      }
+
       </div>
     );
+  }
+
+  ocultar(e){
+
+      this.setState({
+        valor:e.target.checked
+      })
   }
 }
 
