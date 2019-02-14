2a28373366eb7f1f8477033728704ca79f56002f
unknown <estebanbotero1036@gmail.com>
2019-02-14 17:40:28 -0500

    4 intento

--- src/App.js
+++ src/App.js
@@ -6,22 +6,16 @@ class App extends Component {
       super();
 
       this.state = {
-        valor:false
+        valor: false
       }
   }
 
-
   render() {
+    const style = (this.state.valor === false) ? {display:"none"} : {};
     return (
       <div className="wrapper">
-        <label><input type="checkbox" checked={this.state.valor} onClick={this.ocultar.bind(this)}/> Mostrar información importante</label>
-      {
-        this.state.valor ?
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        :
-        null
-      }
-
+        <label><input type="checkbox" checked={this.state.valor} onChange={this.ocultar.bind(this)}/> Mostrar información importante</label>
+        <p style={style}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
     );
   }
