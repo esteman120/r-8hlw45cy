74ffb757e27a5f00e7aa91ae4e32057e77da45a4
unknown <estebanbotero1036@gmail.com>
2019-02-14 17:10:07 -0500

    Segundo montaje

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <label><input type="checkbox" onClick={this.ocultar.bind(this)}/> Mostrar información importante</label>
+        <label><input type="checkbox" checked={this.state.valor} onClick={this.ocultar.bind(this)}/> Mostrar información importante</label>
       {
         this.state.valor ?
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
